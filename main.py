5217cba7ad7dff094530af38a373b5557579aefb
Sharat Bhat <45495724+Sharat-Bhat@users.noreply.github.com>
2022-10-09 21:35:39 -0700

    Comments added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from result import *
 import numpy as np
 
-INF = 1000000000
+INF = 1e12
 
 def esdf_1d(grid):
     """Calculates the distance to the closest obstacle along the first axis
@@ -11,7 +11,6 @@ def esdf_1d(grid):
         grid (float64): Holds the upper bound on the nearest obstacle distance along the axis
     """
     N = grid.shape[0]
-    # print(N)
     k = np.zeros(grid.shape[1:]).astype(np.int32) # Index of rightmost element in lower envelope
     v = np.ones(grid.shape)*INF # Locations of parabolas in lower envelope
     v[0,...] = 0
@@ -19,35 +18,23 @@ def esdf_1d(grid):
     z[0,...] = -INF
     z[1,...] = INF
     
+    # Note that the grid values (0 if obstacle, otherwise infty) if the function f in this case
+    # The modifications made in this loop have also been made in the next loop
     for q in range(1, N):
-        # print(q)
-        v_k = np.take_along_axis(v, k[None, :], axis=0).astype(np.int32)
-        # print(v_k)
-        grid_v_k = np.take_along_axis(grid, v_k, axis=0)
-        # print(grid_v_k)
+        v_k = np.take_along_axis(v, k[None, :], axis=0).astype(np.int32) # Get value of v at corresponding k indices
+        grid_v_k = np.take_along_axis(grid, v_k, axis=0) # Get the values of grid at the corresponding k values
         s = ((grid[q,...] + q*q) - (grid_v_k + v_k*v_k))/(2*q - 2*v_k)
-        # print(s)
-        z_k = np.take_along_axis(z, k[None, :], axis=0)
-        # print(z_k)
+        z_k = np.take_along_axis(z, k[None, :], axis=0) # Get the value of z at corresponding k indices
         mask = (s <= z_k)
-        # print(mask)
-        # print(k)
-        while mask.any() and np.amin(k):
+        while mask.any():
+            # Update k values of those 1D-arrays whose value is now less than of equal to z
+            # Loop won't terminate till all the 1D arrays satisfies the condition
             k[np.squeeze(mask, axis=0)] -= 1
-            # k = np.where(np.squeeze(mask, axis=0), k-1, k)
             v_k = np.take_along_axis(v, k[None, :], axis=0).astype(np.int32)
-            # print(v_k)
             grid_v_k = np.take_along_axis(grid, v_k, axis=0)
-            # print(grid_v_k)
             s = ((grid[q,...] + q*q) - (grid_v_k + v_k*v_k))/(2*q - 2*v_k)
-            # print(s)
             z_k = np.take_along_axis(z, k[None, :], axis=0)
-            # print(z_k)
             mask = (s <= z_k)
-            # print(mask)
-            # print(k)
-            
-        
         k = k+1 
         np.put_along_axis(v, k[None, :], q, axis=0)
         np.put_along_axis(z, k[None, :], s, axis=0)
@@ -57,24 +44,17 @@ def esdf_1d(grid):
     
     ans = np.zeros(grid.shape)
     for q in range(N):
-        # print("Q", q)
         k_1 = k+1
         z_k_1 = np.take_along_axis(z, k_1[None, :], axis=0)
-        # print(z_k_1)
         mask = (z_k_1 < q)
-        # print(mask)
         while mask.any():
             k[np.squeeze(mask, axis=0)] += 1
-            # k = np.where(np.squeeze(mask, axis=0), k-1, k)
             k_1 = k+1
             z_k_1 = np.take_along_axis(z, k_1[None, :], axis=0)
-            # print(z_k_1)
             mask = (z_k_1 < q)
-            # print(mask)
         v_k = np.take_along_axis(v, k[None, :], axis=0).astype(np.int32)
         ans[q,...] = (q - v_k)*(q-v_k) + np.take_along_axis(grid, v_k, axis=0)
         
-    # print(ans)
     return ans
 
 def esdf(M, N, obstacle_list):
@@ -86,22 +66,16 @@ def esdf(M, N, obstacle_list):
     """
     grid = np.ones((M,N))*INF
     obstacle_list = np.array(obstacle_list)
-    # print(obstacle_list)
     grid[tuple(obstacle_list.T)] = 0
-    # print(grid)
-    df1 = esdf_1d(grid)
+    df1 = esdf_1d(grid) # Gets the 1D-array ESDF along the columns 
     df = np.ones(grid.shape)*INF
-    # print(df1)
     grid_x = np.ones(grid.shape)*np.array(range(N))
-    # print(grid_x)
     for q in range(N):
-        dist_x_2 = (grid_x - q)*(grid_x-q)
-        df_q = df1 + dist_x_2
-        # print(df_q)
+        dist_x_2 = (grid_x - q)*(grid_x-q) # Get distance square along the axis
+        df_q = df1 + dist_x_2 # Get square of total overhead distance to obstacle
         df[...,q] = np.amin(df_q, axis=-1)
     
     df = np.sqrt(df)
-    # print(df)
     return df
 
 if __name__ == '__main__':
